6fd592708ab5aa96f50ed936ce9e163cea1c992a
Marcos Rachid <marcosrachid@gmail.com>
2019-07-29 18:56:44 -0300

    docker unnecessary

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -p 127.0.0.1:8001:8001 kubernetes-hyperledger-az-manager
+docker run -it -p 127.0.0.1:8001:8001 kubernetes-hyperledger-az-manager
